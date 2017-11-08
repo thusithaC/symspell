869a283f5610b7307006ec21bd3844f5986a360e
Sebastian Rose <rose@semkath.de>
2017-11-08 20:58:17 +0100

    Use a more complicated example for suffix shortening

--- src/test/scala/de/semkath/symspell/cleaning/SuffixRemovalTest.scala
+++ src/test/scala/de/semkath/symspell/cleaning/SuffixRemovalTest.scala
@@ -8,7 +8,7 @@ class SuffixRemovalTest extends FlatSpec {
     behavior of "SuffixRemoval"
 
     it should "removeLegalSuffixes" in {
-        val shortened = suffixRemoval.removeLegalSuffixes("rose limited incorporated")
+        val shortened = suffixRemoval.removeLegalSuffixes("rose limited kabushiki kaisha gesellschaft mit beschraenkter haftung")
         assert(shortened == "rose")
     }
 
